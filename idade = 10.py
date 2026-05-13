6a4ad56eb627e7fd746157fa0f087077e12e9405
edimones001 <edimones@gmail.com>
2026-05-13 09:54:36 -0300

    atual

---
+++ idade = 10.py
@@ -0,0 +1,8 @@
+idade = 10
+if idade >= 18:
+    print('É maior de idade.')
+else:
+    print('Não é maior de idade.')   
+
+    
+               
---
+++ idade = 18.py
@@ -0,0 +1,6 @@
+idade = 25
+tem_carteira_motorista = False
+if idade >=18 and tem_carteira_motorista == True:
+    print('pessoa pode dirigir!')
+else:
+    print('pessoa não pode dirigir!') 
